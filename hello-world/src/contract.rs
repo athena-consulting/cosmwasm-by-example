2a4e0905662d0842312faa128bbbeeaec9e128c1
gachouchani1999 <gachouchani@ndu.edu.lb>
2023-03-02 17:59:25 +0200

    fixed hello-world smart contract instantiate and execute functions so it can be deployed successfully'

--- hello-world/src/contract.rs
+++ hello-world/src/contract.rs
@@ -19,7 +19,7 @@ pub fn instantiate(
     _info: MessageInfo,
     _msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
+    Ok(Response::default())
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -29,7 +29,7 @@ pub fn execute(
     _info: MessageInfo,
     _msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
+    Ok(Response::default())
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
